43723d4cc8beeaa48b908a109e1c1e87e154e58d
Emre Buyuker <emrebykr66@gmail.com>
2019-03-16 01:34:33 +0300

    Initial commit

---
+++ app/src/main/java/com/buyukeryazilim/winpolmobile/AdminPage.java
@@ -0,0 +1,59 @@
+package com.buyukeryazilim.winpolmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    EditText emailText;
+    EditText passwordText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+    }
+
+    public void signUp (View view) {
+
+        mAuth.createUserWithEmailAndPassword(emailText.getText().toString(), passwordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(),"User Created",Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                            startActivity(intent);
+
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(AdminPage.this,e.getLocalizedMessage().toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
